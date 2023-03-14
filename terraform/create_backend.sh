0c463a376b2c2e99fb6fd0c8b8cefe526e22578c
Oluwaseun <oluseun.ismaila@gmail.com>
2023-03-14 14:45:32 -0500

    modified pipleine

--- terraform/create_backend.sh
+++ terraform/create_backend.sh
@@ -11,11 +11,23 @@ tenantid="c579f8a6-171d-4be4-ba33-68b16a8ae922"
 
 az login --service-principal --tenant $tenantid --username $appid --password $password
 
+# Check if pipeline variables already exist
+if [ -n "$backend_resource_group" ] && [ -n "$backend_storage_account" ] && [ -n "$backend_container" ]; then
+    echo "Terraform backend already configured. Skipping backend creation."
+    exit 0
+fi
+
 # Check if storage account already exists
 if az storage account check-name --name $storageAccountName --query 'nameAvailable' | grep -q false; then
-    echo "Storage account $storageAccountName already exists. Exiting script."
+    echo "Storage account $storageAccountName already exists. Skipping backend creation."
     echo "If you are running this pipeline for the first time, please provide a unique storage account name."
     echo "If you are running this pipeline after your first time, please ignore. Your terraform backend has already been configured."
+    
+    # Set pipeline variables for backend configuration
+    echo "##vso[task.setvariable variable=backend_resource_group]$(az storage account show --name $storageAccountName --query 'resourceGroup' -o tsv)"
+    echo "##vso[task.setvariable variable=backend_storage_account]$storageAccountName"
+    echo "##vso[task.setvariable variable=backend_container]$containerName"
+    
     exit 0
 fi
 

c23e707d32100541bdc444100637c130e555ba29
Oluwaseun <oluseun.ismaila@gmail.com>
2023-03-14 14:27:17 -0500

    added backend config

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+   backend "azurerm" {
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_key
+    resource_group_name  = var.backend_resource_group_name
+  }
 }
 
 # Configure the Microsoft Azure Provider

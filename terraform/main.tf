f8f97fe2315c62b5300dd7d90d9aa7f92cddf289
Oluwaseun <oluseun.ismaila@gmail.com>
2023-03-16 18:48:35 -0500

    added admin enabled

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   sku                 = "Premium"
-  admin_enabled       = false
+  admin_enabled       = true
 
 }
 

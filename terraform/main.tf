7b453a522d0873ed6ec746ed5a9bca79d76ab965
Oluwaseun <oluseun.ismaila@gmail.com>
2023-03-17 08:20:26 -0500

    removed external db

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,28 +35,6 @@ resource "azurerm_container_registry" "acr" {
 }
 
 
-# Mysql server
-resource "azurerm_mysql_server" "database" {
-  name                = "${var.unique_var}-mysqlserver"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
-
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-}
-
 # Kubernetes
 
 resource "azurerm_kubernetes_cluster" "aks" {
